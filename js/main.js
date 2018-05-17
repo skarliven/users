743a16eeb20694dc469477d55da7c79a5153b853
Skarli Pena <skarlicodemusic@gmail.com>
2018-05-17 12:15:57 -0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("connected");
+var userFirstName = prompt("What's your name?");
+var userLastName = prompt("What's your last name");
+var userAge = prompt(userFirstName + " " + "Please enter your age");
+var greeting = alert("Hello " + userFirstName + " " + userLastName + ".");
+
+
+var monthsAlive = userAge * 12; 
+var daysAlive = 365 * userAge;
+document.write('Wow You are' + " " + monthsAlive + ' months old' + "," + ' and' + " " + daysAlive + ' days old!');
